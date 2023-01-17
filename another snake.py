ac47fd46ac4b4dc6bb8ccff30e0f24e36518b360
fatihthefastmen <122343971+fatihthefastmen@users.noreply.github.com>
2023-01-17 14:53:45 +0700

    Add files via upload

---
+++ another snake.py
@@ -0,0 +1,24 @@
+tugas = float(input("masukan nilai tugas: "))
+uts = float(input("masukan nilai uts: "))
+uas = float(input("masukan nilai uas: "))
+
+nilai = (0.15 * tugas) + (0.30 * uts) + (0.50 * uas)
+
+if nilai > 80:
+    grade = 'A'
+elif nilai > 70:
+    grade = 'B'
+elif nilai > 60:
+    grade = 'C'
+elif nilai > 50:
+    grade = "D"
+else:
+    grade = 'E'
+
+if nilai > 60:
+    status = 'lulus'
+else:
+    status = 'tidak lulus'
+print('Nilai akhir : %0.2f' % nilai)
+print('grade:{}'.format(grade))
+print('status:{}'.format(status))
